87cdc86a6d7db8b3d596df2a65ee92a6500e85e0
lucasmpavelski <lmpavelski@yahoo.com.br>
2018-01-24 10:45:57 -0200

    libraries added

---
+++ lib/hv/CMakeFiles/hv.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/lucasmp/projects/elmoead_gpu/lib/hv/avl.c" "/home/lucasmp/projects/elmoead_gpu/lib/hv/CMakeFiles/hv.dir/avl.c.o"
+  "/home/lucasmp/projects/elmoead_gpu/lib/hv/hv.c" "/home/lucasmp/projects/elmoead_gpu/lib/hv/CMakeFiles/hv.dir/hv.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "EIGEN_NO_DEBUG"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "lib/gtest"
+  "lib/hv/hv.h"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ lib/hv/CMakeFiles/hv.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hv.dir/hv.c.o"
+  "CMakeFiles/hv.dir/avl.c.o"
+  "libhv.pdb"
+  "libhv.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/hv.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
